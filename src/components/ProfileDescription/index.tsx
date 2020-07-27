31cb08c9256504a9bd068c0ad33aa67af27dda30
Christian Wong <dev@christianwong.com>
2020-07-27 20:17:14 -0300

    Label enhancement.

--- src/components/ProfileDescription/index.tsx
+++ src/components/ProfileDescription/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProfileDescription: React.FC = () => {
   return (
     <>
-      <p>Hello, world! I am a developer and work as Scrum Master at Amdocs.</p>
+      <p>Hello! I am a developer and work as Scrum Master at Amdocs.</p>
       <p>
         I got my master degree in Computer Science at University of São Paulo.
       </p>
