ccef678b60eb246728231387fb83be50e067f67e
Christian Wong <dev@christianwong.com>
2020-07-29 17:28:49 -0300

    Adding footer. Issue#5

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FooterDark = styled.div`
+  background-color: #45969b;
+  padding: 20px 40px;
+  color: #f2eaec;
+  font-size: calc(8px + 1vmin);
+  border-top: 5px solid #f2eaec;
+
+  a {
+    color: #0ce5e1;
+  }
+`;
